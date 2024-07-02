7c343d902b59fd4e4f0e5a0376197c2edb13594c
Raja kumar <rajaraja360parth@gmail.com>
2024-07-02 12:26:13 +0530

    Routing concept

---
+++ svelte-tutorial/src/lib/Table.svelte
@@ -0,0 +1,7 @@
+<h1>This <code>Table.svelte</code> file will not create any link.It needs to be imported in other file to access.</h1>
+
+<!--
+(1).Each page of your app is a Svelte component.
+(2).Create pages by adding files to the scr/routes directory of your project.
+(3).Lib contains your library code(js utilites and components),which can be imported via the $lib alias
+-->
---
+++ svelte-tutorial/src/routes/blog/list/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    import Table from "$lib/Table.svelte"
+</script>
+
+<h1>This is a list of blogs.</h1>
+<Table/>
